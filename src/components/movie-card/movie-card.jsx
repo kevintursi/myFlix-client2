4bfd38045e9159bc1e08b79d06fc6cc068031f5d
kevintursi <kjt923@gmail.com>
2022-12-14 17:17:33 -0500

    Added Requirements for Prop-Types from API

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -8,4 +10,19 @@ export const MovieCard = ({ movie, onMovieClick }) => {
             {movie.Title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        director: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        bio: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
